97e6d37033d18ed4340a5614bba569a51dd45e98
Kåre Joakim Jørgensen <kaare1994@gmail.com>
2017-09-15 13:24:56 +0200

    Added type to sensor, and added Q-motion thingtype

--- org.openhab.binding.northqbinding/src/main/java/org/openhab/binding/northqbinding/models/BinarySensor.java
+++ org.openhab.binding.northqbinding/src/main/java/org/openhab/binding/northqbinding/models/BinarySensor.java
@@ -8,12 +8,14 @@ import com.google.gson.reflect.TypeToken;
 public class BinarySensor {
     public static final Type gsonType = new TypeToken<List<BinarySensor>>() {
     }.getType();
-    private int node_id;
+    private long uploaded;
     private int room;
     private int battery;
+    private int pos;
     private String name;
     private int armed;
     private List<Sensor> sensors;
+    private int node_id;
     private String gateway;
 
     public int getNode_id() {
@@ -72,6 +74,18 @@ public class BinarySensor {
         this.gateway = gateway;
     }
 
+    public long getUploaded() {
+        return uploaded;
+    }
+
+    public void setUploaded(long uploaded) {
+        this.uploaded = uploaded;
+    }
+
+    public boolean isMotionDetected() {
+        return pos == 255;
+    }
+
     @Override
     public String toString() {
         return "BinarySensor{" + "node_id=" + node_id + ", room=" + room + ", battery=" + battery + ", name='" + name
@@ -80,6 +94,25 @@ public class BinarySensor {
     }
 
     public static class Sensor {
+        public enum Type {
+            TEMPERATURE,
+            lUMINANCE,
+            HUMIDITY;
+        }
+
+        private Type getType(int type) {
+            switch (type) {
+                case 1:
+                    return Type.TEMPERATURE;
+                case 3:
+                    return Type.lUMINANCE;
+                case 5:
+                    return Type.HUMIDITY;
+                default:
+                    return null;
+            }
+        }
+
         private int scale;
         private int type;
         private double value;
@@ -92,8 +125,8 @@ public class BinarySensor {
             this.scale = scale;
         }
 
-        public int getType() {
-            return type;
+        public Type getType() {
+            return getType(type);
         }
 
         public void setType(int type) {
